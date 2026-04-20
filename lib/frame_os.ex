8d11544872eee8c1059354062a15e1c1e574d62d
Davide Bettio <davide@uninstall.it>
2026-04-20 12:42:57 +0200

    First commit
    mix new --module FrameOS frame_os

---
+++ lib/frame_os.ex
@@ -0,0 +1,18 @@
+defmodule FrameOS do
+  @moduledoc """
+  Documentation for `FrameOS`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FrameOS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/frame_os_test.exs
@@ -0,0 +1,8 @@
+defmodule FrameOSTest do
+  use ExUnit.Case
+  doctest FrameOS
+
+  test "greets the world" do
+    assert FrameOS.hello() == :world
+  end
+end
